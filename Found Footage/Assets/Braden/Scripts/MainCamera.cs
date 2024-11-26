a6ce56f592df2ad37472193a8ab5372773135566
Braden Nichols <1719263@mukilteo.wednet.edu>
2024-11-26 13:38:36 -0800

    player movement with controller support

--- Found Footage/Assets/Braden/Scripts/MainCamera.cs
+++ Found Footage/Assets/Braden/Scripts/MainCamera.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MainCamera : MonoBehaviour
 {
     public float sensitivity;
     public Transform orientation;
 
-    private float xRotation;
-    private float yRotation;
+    float xRotation;
+    float yRotation;
+
+    Vector2 lookAxis = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +21,25 @@ public class MainCamera : MonoBehaviour
     }
 
     // Update is called once per frame
+    
     void Update()
     {
-        // mouse
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * (sensitivity * 400);
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * (sensitivity * 400);
+        // axis
+        float axisX = lookAxis.x * Time.deltaTime * (sensitivity * 6);
+        float axisY = lookAxis.y * Time.deltaTime * (sensitivity * 6);
 
-        yRotation += mouseX;
-        xRotation = Mathf.Clamp(xRotation - mouseY, -90f, 90f);
+        yRotation += axisX;
+        xRotation = Mathf.Clamp(xRotation - axisY, -90f, 90f);
 
         // rotate
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    public void LookEvent(InputAction.CallbackContext context)
+    {
+        lookAxis = context.ReadValue<Vector2>();
+
+        //print(lookAxis);
     }
 }
