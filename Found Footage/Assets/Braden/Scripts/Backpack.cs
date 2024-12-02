aacab6bff557ddeb106fdc1760790ed546f3ed13
Braden Nichols <1719263@mukilteo.wednet.edu>
2024-12-02 10:23:26 -0800

    backpack system + base protogeist

--- Found Footage/Assets/Braden/Scripts/Backpack.cs
+++ Found Footage/Assets/Braden/Scripts/Backpack.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Backpack : MonoBehaviour
 {
+    [Header("References")]
+    public Transform cameraTransform;
+
+    [Header("Runtime")]
+    [HideInInspector]
     public GameObject heldItem;
+    ItemData heldItemData;
+
+    [Header("Settings")]
+    public Vector3 defaultHoldOffset;
+    public float itemLerpSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         // transform.parent = GameObject.Find("Floor").transform;
+
+        GameObject testItem = GameObject.Find("Protogeist");
+        AddItem(testItem);
+        EquipItem(testItem);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (heldItem != null)
+        {
+            Vector3 itemOffset = defaultHoldOffset;
+            if (heldItemData) itemOffset = heldItemData.holdingOffset;
+
+            // Lerp Item Position
+
+            Vector3 newPosition = cameraTransform.position +
+                (cameraTransform.forward * itemOffset.z + cameraTransform.right * itemOffset.x + cameraTransform.up * itemOffset.y);
+
+            heldItem.transform.rotation = cameraTransform.rotation;
+            heldItem.transform.position = Vector3.Lerp(heldItem.transform.position, newPosition, Time.deltaTime * itemLerpSpeed);
+        }
     }
+
+    // Public Functions
 
     public List<GameObject> GetItems()
     {
@@ -31,16 +59,26 @@ public class Backpack : MonoBehaviour
     public void AddItem(GameObject item)
     {
         item.transform.parent = transform; // parent it to backpack
+        item.SetActive(false);
     }
 
     public void EquipItem(GameObject item)
     {
-        if (heldItem == item)
-        {
-            // unequip
-        } else
-        {
+        if (heldItem == item) return;
+        else if (heldItem != null) UnequipItem();
 
+        heldItem = item;
+        heldItemData = item.GetComponent<ItemData>();
+        item.SetActive(true);
+    }
+
+    public void UnequipItem()
+    {
+        if (heldItem != null)
+        {
+            heldItem.SetActive(false);
         }
+
+        heldItem = null;
     }
 }
