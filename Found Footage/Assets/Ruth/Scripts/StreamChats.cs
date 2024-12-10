c72c8cb7bad1ec7083040e83f7c05d6eb7809096
Braden Nichols <1719263@mukilteo.wednet.edu>
2024-12-10 10:10:45 -0800

    chat first version

--- Found Footage/Assets/Ruth/Scripts/StreamChats.cs
+++ Found Footage/Assets/Ruth/Scripts/StreamChats.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 
@@ -11,5 +13,31 @@ public struct ChatMessage
 }
 public class StreamChats : MonoBehaviour
 {
+    public GameObject exampleMessage;
     public List<ChatMessage> messages;
+
+    private void Start()
+    {
+        for (int i = 0; i < messages.Count; i++)
+            CreateMessage(i);
+
+        // TODO later: when the first message Y pos changes, delete it (since we'd hit the max)
+    }
+
+    public GameObject CreateMessage(int messageIndex)
+    {
+        ChatMessage messageData = messages[messageIndex];
+
+        GameObject messageObject = Instantiate(exampleMessage, transform);
+        TMP_Text textMeshPro = messageObject.GetComponent<TMP_Text>();
+
+        textMeshPro.text = $"{messageData.username}: {messageData.message}";
+
+        messageObject.name = messageIndex.ToString();
+        messageObject.SetActive(true);
+
+        Debug.Log($"Create message (index {messageIndex.ToString()})");
+
+        return messageObject;
+    }
 }
