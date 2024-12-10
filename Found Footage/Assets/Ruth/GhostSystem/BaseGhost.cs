e2a9582ae8ccfaefde40300a578eb22a21b97b3d
Braden Nichols <1719263@mukilteo.wednet.edu>
2024-12-10 09:54:47 -0800

    fixed ghost related issues

---
+++ Found Footage/Assets/Ruth/GhostSystem/BaseGhost.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseGhost : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    public int resistance;
+    public GameObject target;
+}
---
+++ Found Footage/Assets/Ruth/GhostSystem/Gjenganger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gjenganger : BaseGhost
+{
+    void Start()
+    {
+
+    }
+}
