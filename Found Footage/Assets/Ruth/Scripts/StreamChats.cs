fb7f5cca31bde834cff450ccf9a966c25c61a1c9
Braden Nichols <1719263@mukilteo.wednet.edu>
2024-12-11 10:15:51 -0800

    i tried but the chat is just not working with me

--- Found Footage/Assets/Ruth/Scripts/StreamChats.cs
+++ Found Footage/Assets/Ruth/Scripts/StreamChats.cs
@@ -14,7 +14,10 @@ public struct ChatMessage
 public class StreamChats : MonoBehaviour
 {
     public GameObject exampleMessage;
+    public GameObject content;
     public List<ChatMessage> messages;
+
+    //private Queue chatQueue = new();
 
     private void Start()
     {
@@ -24,11 +27,31 @@ public class StreamChats : MonoBehaviour
         // TODO later: when the first message Y pos changes, delete it (since we'd hit the max)
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            CreateMessage(0); // testing keybind
+    }
+
     public GameObject CreateMessage(int messageIndex)
     {
+        // Old Message
+
+        //GameObject oldestMessage;
+        /*
+        float oldYPos = 0;
+
+        if (chatQueue.Count > 0)
+        {
+            //oldestMessage = (GameObject)chatQueue.Peek();
+            oldYPos = ((GameObject)chatQueue.Peek()).GetComponent<RectTransform>().rect.y;
+        }*/
+
+        // Create
+
         ChatMessage messageData = messages[messageIndex];
 
-        GameObject messageObject = Instantiate(exampleMessage, transform);
+        GameObject messageObject = Instantiate(exampleMessage, content.transform);
         TMP_Text textMeshPro = messageObject.GetComponent<TMP_Text>();
 
         textMeshPro.text = $"{messageData.username}: {messageData.message}";
@@ -36,7 +59,20 @@ public class StreamChats : MonoBehaviour
         messageObject.name = messageIndex.ToString();
         messageObject.SetActive(true);
 
-        Debug.Log($"Create message (index {messageIndex.ToString()})");
+        // After
+
+        //Debug.Log($"Create message (index {messageIndex.ToString()})");
+        /*chatQueue.Enqueue(messageObject);
+
+        Debug.Log(oldYPos);
+        Debug.Log(((GameObject)chatQueue.Peek()).GetComponent<RectTransform>().rect.y);
+
+        if (chatQueue.Count > 1 && oldYPos != ((GameObject)chatQueue.Peek()).GetComponent<RectTransform>().rect.y)
+        {
+            Destroy((GameObject)chatQueue.Dequeue());
+
+            Debug.Log("DESTROYED!");
+        }*/
 
         return messageObject;
     }
