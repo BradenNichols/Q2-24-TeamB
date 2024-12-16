5175f99883448553ff206abdf06ce045b3a6e58d
MUKILTEO\1717998 <1717998@mukilteo.wednet.edu>
2024-12-16 10:12:03 -0800

    just dance  people

---
+++ Found Footage/Assets/Ruth/Scripts/GhostSystem/LadyInRed.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadyInRed : MonoBehaviour
+{
+    public int speed;
+    public int health;
+    public int resistance;
+    public int damgage;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
