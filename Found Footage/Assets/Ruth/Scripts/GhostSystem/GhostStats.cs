e0a2782ea73d548dc9162a4180477842d92acd31
MUKILTEO\1717998 <1717998@mukilteo.wednet.edu>
2024-12-17 09:05:31 -0800

    glungulus

--- Found Footage/Assets/Ruth/Scripts/GhostSystem/GhostStats.cs
+++ Found Footage/Assets/Ruth/Scripts/GhostSystem/GhostStats.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LadyInRed : GhostStats
+public class GhostStats : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int speed;
+    public int damage;
+    public int resistance;
+    public GameObject target;
 
     // Update is called once per frame
     void Update()
