a84b9e4981d2f8256ba2fc4503d5e302d8ad3ba2
MUKILTEO\1719263 <1719263@mukilteo.wednet.edu>
2025-01-09 10:20:29 -0800

    almost done with pause screen

--- Found Footage/Assets/Braden/Scripts/MainCamera.cs
+++ Found Footage/Assets/Braden/Scripts/MainCamera.cs
@@ -21,13 +21,6 @@ public class MainCamera : MonoBehaviour
 
     Vector2 lookAxis = Vector2.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
     // Update is called once per frame
     
     void Update()
---
+++ Found Footage/Assets/Braden/Scripts/PauseScript.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    [Header("State")]
+    public bool canPause = true;
+    public bool isPaused = false;
+
+    [Header("References")]
+    public GameObject pauseMenu;
+    public InputActionReference pauseAction;
+
+    [Header("Pixel References")]
+    public RawImage pixelsImage;
+    public Camera playerCamera;
+    public RenderTexture lowPixels;
+    public RenderTexture medPixels;
+
+    // Input
+    void Start()
+    {
+        pauseAction.action.started += PauseInputEvent;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void PauseInputEvent(InputAction.CallbackContext context)
+    {
+        SetPaused(!isPaused);
+    }
+
+    // Main
+    public void SetPaused(bool paused = true)
+    {
+        if (paused == isPaused) return;
+
+        if (paused)
+        {
+            if (!canPause) return;
+
+            pauseMenu.SetActive(true);
+
+            playerCamera.targetTexture = medPixels;
+            pixelsImage.texture = medPixels;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+
+            playerCamera.targetTexture = lowPixels;
+            pixelsImage.texture = lowPixels;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            Time.timeScale = 1;
+        }
+
+        isPaused = paused;
+    }
+
+    // Buttons
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Continue()
+    {
+        SetPaused(false);
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
