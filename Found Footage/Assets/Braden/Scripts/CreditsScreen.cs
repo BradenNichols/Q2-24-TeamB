d62998983b042f019b1cc5f5389b3e434dd18f43
Braden Nichols <1719263@mukilteo.wednet.edu>
2025-01-10 17:25:29 -0800

    credits scene (needs images for each person)

---
+++ Found Footage/Assets/Braden/Scripts/CreditsScreen.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class CreditsScreen : MonoBehaviour
+{
+    public InputActionReference escapeInput;
+    public InputActionReference nextSlideInput;
+    public InputActionReference lastSlideInput;
+    public List<Image> slides;
+
+    Image pastSlide;
+    int slideIndex;
+
+    void Start()
+    {
+        escapeInput.action.started += EscapeEvent;
+        nextSlideInput.action.started += NextSlideEvent;
+        lastSlideInput.action.started += LastSlideEvent;
+
+        slideIndex = Random.Range(0, slides.Count - 1);
+        NextSlide(0);
+    }
+
+    // Input
+    public void NextSlideEvent(InputAction.CallbackContext context) { NextSlide(1); }
+    public void LastSlideEvent(InputAction.CallbackContext context) { NextSlide(-1); }
+    public void EscapeEvent(InputAction.CallbackContext context) { Exit(); }
+
+    // Functions
+    public void Exit()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    void NextSlide(int indexChange)
+    {
+        slideIndex += indexChange;
+
+        if (slideIndex < 0)
+            slideIndex = slides.Count - 1;
+        else if (slideIndex >= slides.Count)
+            slideIndex = 0;
+
+        if (pastSlide)
+            pastSlide.enabled = false;
+
+        Image currentSlide = slides[slideIndex];
+        currentSlide.enabled = true;
+
+        pastSlide = currentSlide;
+    }
+}
